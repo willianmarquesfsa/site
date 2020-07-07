e43efb83a836ea330873b00e20d6ed7fc1d96d6a
willianmarquesfsa <willianmarques_fsa@hotmail.com>
2020-07-07 18:18:29 -0300

    frontend0707bnvneea101aasaaasadfg

--- src/pages/services/api.js
+++ src/pages/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:  'http://192.168.0.100:3000/'   //  'http://classificasdosfsa-com.umbler.net',
+    baseURL:  'http://classificasdosfsa-com.umbler.net'   //  'http://classificasdosfsa-com.umbler.net',
 })
 
 export default api;
